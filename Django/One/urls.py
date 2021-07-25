9c26f6df0568d051de6d17ec9e0b0e997682f954
Tanya9058 <tanja9058@mail.ru>
2021-07-25 11:45:59 +0300

    project Django

--- Django/One/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from One.views import MyClass, MyClassTwo
-
-urlpatterns =[
-    path('', MyClass.as_view()),  # статус-код 400
-    # path('', MyClassTwo.as_view())  # статус-код 201
-]
--- Django/config/urls.py
+++ Django/config/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from One.views import MyClass, MyClassTwo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('One.urls'))
+    path('error/', MyClass.as_view()),
+    path('success/', MyClassTwo.as_view())
 ]
